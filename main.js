c8d495e1ab7577ae22da271fbf80dc85d2565b36
brittanyweickert <brittanyweickert@gmail.com>
2019-10-15 20:46:29 -0400

    css changes and api adjustments

--- main.js
+++ main.js
@@ -11,13 +11,10 @@ function handleFormSubmit() {
         let searchTerm = $('#videos-search-field').val() + 'repair';
         // if we want to set a limit later on
         let maxResults = 4;
-        // When we get zipcode from form
-        let zip = 33610;
-
-
+        
         if (searchTerm !== '') {
             getYouTubeVideos(searchTerm, maxResults);
-            getMapData(zip);
+            getMapData();
         } else {
             alert('please enter a repair you would like to learn about');
         }
@@ -53,12 +50,13 @@ function getYouTubeVideos(searchTerm, resultsMax = 10) {
 
 function displayYouTubeResults(responseJson) {
     $('#videos-list').empty();
+    console.log(responseJson);
     for (let i = 0; i < responseJson.items.length; i++) {
         $('#videos-list').append(
             `<li>
                 <h3>${responseJson.items[i].snippet.title}</h3>
                 <p>${responseJson.items[i].snippet.description}</p>
-                <img src='${responseJson.items[i].snippet.thumbnails.default.url}'
+                <img src='${responseJson.items[i].snippet.thumbnails.high.url}'
             </li>
             `
         )
@@ -67,40 +65,40 @@ function displayYouTubeResults(responseJson) {
     $('#videos-list').removeClass('hidden');
 }
 
-// This will be the section for locations API
+// // This will be the section for locations API
 
-const clientID = 'YMBYSODCXL3DCEIJGJIW2N5EGME0O10PVDF2A41Z1MIP0KZD';
-const clientSecret = 'LIWLXKL0O1ASSEMUWFC15SUTCU4WK1PJXBNLUHTRCJQW5BWW';
-const fourSquareURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientID}&client_secret=${clientSecret}&v=20180323&limit=5&ll=40.7243,-74.0018&query=coffee`
+// const clientID = 'YMBYSODCXL3DCEIJGJIW2N5EGME0O10PVDF2A41Z1MIP0KZD';
+// const clientSecret = 'LIWLXKL0O1ASSEMUWFC15SUTCU4WK1PJXBNLUHTRCJQW5BWW';
+// const fourSquareURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientID}&client_secret=${clientSecret}&v=20180323&limit=5&ll=40.7243,-74.0018&query=coffee`
 
-function getMapData() {
-fetch(fourSquareURL).then(res => {
-    if (res.ok) {
-        return res.json();
-    } else {
-        throw new Error(res.statusText);
-    }
-})
-.then(res => {
-    console.log(res)
-    displayMap(res);
-})
-.catch(err => {
-    console.log(err.statusText)
-});
-}
+// function getMapData() {
+// fetch(fourSquareURL).then(res => {
+//     if (res.ok) {
+//         return res.json();
+//     } else {
+//         throw new Error(res.statusText);
+//     }
+// })
+// .then(res => {
+//     console.log(res)
+//     displayMap(res);
+// })
+// .catch(err => {
+//     console.log(err.statusText)
+// });
+// }
 
-function displayMap(mapData) {
-    $('#info-holder').empty();
-    for (let i = 0; i < mapData.groups.items.length; i++) {
-        $('#info-holder').append(
-            `<li>
-                <h3>${mapData.groups.items[i].venue.name}</h3>
-                <p>${mapData.groups.items[i].venue.location}</p>            
-            </li>
-            `
-        )
-    }
+// function displayMap(mapData) {
+//     $('#info-holder').empty();
+//     for (let i = 0; i < mapData.groups.items.length; i++) {
+//         $('#info-holder').append(
+//             `<li>
+//                 <h3>${mapData.groups.items[i].venue.name}</h3>
+//                 <p>${mapData.groups.items[i].venue.location}</p>            
+//             </li>
+//             `
+//         )
+//     }
 
-    $('#info-holder').removeClass('hidden');
-}
+//     $('#info-holder').removeClass('hidden');
+// }
