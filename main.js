c6dcdde51968cc67ed7f849c9bda324e091ae02c
Ryan Kadel <ryan.kadel88@gmail.com>
2019-10-19 13:35:55 -0400

    fixed minimeme

--- main.js
+++ main.js
@@ -120,9 +120,9 @@ function initMap(venues, start) {
     });
 
     if (venues.response.groups[0].items.length < 1) {
-        $('#map').html(`<h3 class="error">Sorry! We Couldnt Locate any Nearby repair shops for you.</h3>
-                            <img class="error-meme" src="images/phone.jpg">
-                            <p class="error">Please try a different zip code.</p>`);
+        $('#map').html(`<h3 class="error">Sorry! We Couldnt Locate any Nearby repair shops for you.</h3> 
+                            <img class="error-meme" src="images/phone.jpg">                     
+                            <p class="error">Please try a different zip code.</p>`);                
     } else {
         for (let i = 0; i < venues.response.groups[0].items.length; i++) {
             let shortPath = venues.response.groups[0].items[i].venue;
